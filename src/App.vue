1362723fb3da8fe3b340ebd68792b5ebec9a6724
Fernando Orellana <fernandoarieldb95@gmail.com>
2025-01-10 17:22:17 -0300

    empezando proyecto portafolio

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LayoutView from './layout/LayoutView.vue';
+import HomeView from './views/HomeView.vue'
+</script>
+
+<template>
+
+  <LayoutView/>
+  <HomeView />
+</template>
---
+++ src/layout/LayoutView.vue
@@ -0,0 +1,6 @@
+<script setup>
+    
+</script>
+<template>
+    <h1>barra nav</h1>
+</template>
