55db94ad1dd04a992c351fff7e7cc157795b040a
Fernando Orellana <fernandoarieldb95@gmail.com>
2025-01-18 20:30:14 -0300

    armando la vista del proyecto diario-sims

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,28 @@
 <script setup>
-
 import LayoutView from "@/layout/LayoutView.vue";
 import HabilidadView from "@/components/HabilidadView..vue";
 import ProyectPreView from "@/components/ProyectPreView.vue";
-import inicioImage from '@/assets/images/distribuidora/inicio.png';
+import inicioSimsDiary from "@/assets/images/sims-page/inicio.png";
+import inicioDistribuitor from "@/assets/images/distribuidora/inicio.png";
 
+const tecnologiesSimsDiary = [
+  {
+    name: "HTML",
+    src: "https://img.icons8.com/ios/50/ffffff/html-5--v1.png",
+  },
+  {
+    name: "CSS",
+    src: "https://img.icons8.com/ios/50/ffffff/css3.png",
+  },
+  {
+    name: "PHP",
+    src: "https://img.icons8.com/?size=100&id=39852&format=png&color=FFFFFF",
+  },
+  {
+    name: "MySql",
+    src: "https://img.icons8.com/?size=100&id=39855&format=png&color=FFFFFF",
+  },
+];
 const tecnologiesDistributor = [
   {
     name: "HTML",
@@ -27,12 +45,13 @@ const tecnologiesDistributor = [
     src: "https://img.icons8.com/?size=100&id=39855&format=png&color=FFFFFF",
   },
 ];
+
 </script>
 
 <template>
   <div class="bg-gray-900 text-white min-h-screen">
     <!-- Cabecera -->
-    <LayoutView/>
+    <LayoutView />
     <!-- Sección Principal -->
     <section class="py-20 text-center">
       <div class="max-w-2xl mx-auto">
@@ -56,29 +75,54 @@ const tecnologiesDistributor = [
       <div class="max-w-7xl mx-auto text-center">
         <h2 class="text-3xl font-bold text-teal-400 mb-12">Habilidades</h2>
         <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-12">
+          <HabilidadView
+            name="HTML"
+            src="https://img.icons8.com/ios/50/ffffff/html-5--v1.png"
+          />
 
-          <HabilidadView name="HTML" src="https://img.icons8.com/ios/50/ffffff/html-5--v1.png"/>
+          <HabilidadView name="CSS" src="https://img.icons8.com/ios/50/ffffff/css3.png" />
 
-          <HabilidadView name="CSS" src="https://img.icons8.com/ios/50/ffffff/css3.png"/>
+          <HabilidadView
+            name="JavaScript"
+            src="https://img.icons8.com/ios/50/ffffff/javascript.png"
+          />
 
-          <HabilidadView name="JavaScript" src="https://img.icons8.com/ios/50/ffffff/javascript.png"/>
-          
-          <HabilidadView name="PHP" src="https://img.icons8.com/?size=100&id=39852&format=png&color=FFFFFF"/>
+          <HabilidadView
+            name="PHP"
+            src="https://img.icons8.com/?size=100&id=39852&format=png&color=FFFFFF"
+          />
 
-          <HabilidadView name="SQL" src="https://img.icons8.com/?size=100&id=77694&format=png&color=FFFFFF"/>
-         
-          <HabilidadView name="MySQL" src="https://img.icons8.com/?size=100&id=39855&format=png&color=FFFFFF"/>
+          <HabilidadView
+            name="SQL"
+            src="https://img.icons8.com/?size=100&id=77694&format=png&color=FFFFFF"
+          />
 
-          <HabilidadView name="Tailwind" src="https://img.icons8.com/?size=100&id=qOFWMoaAQIdR&format=png&color=FFFFFF"/>
-        
-          <HabilidadView name="Vue.js" src="https://img.icons8.com/ios/50/ffffff/vuejs.png"/>
-          
-          <HabilidadView name="Laravel" src="https://img.icons8.com/ios/50/ffffff/laravel.png" />
+          <HabilidadView
+            name="MySQL"
+            src="https://img.icons8.com/?size=100&id=39855&format=png&color=FFFFFF"
+          />
+
+          <HabilidadView
+            name="Tailwind"
+            src="https://img.icons8.com/?size=100&id=qOFWMoaAQIdR&format=png&color=FFFFFF"
+          />
+
+          <HabilidadView
+            name="Vue.js"
+            src="https://img.icons8.com/ios/50/ffffff/vuejs.png"
+          />
+
+          <HabilidadView
+            name="Laravel"
+            src="https://img.icons8.com/ios/50/ffffff/laravel.png"
+          />
 
           <HabilidadView name="Git" src="https://img.icons8.com/ios/50/ffffff/git.png" />
 
-          <HabilidadView name="Visual Studio Code" src="https://img.icons8.com/?size=100&id=121601&format=png&color=FFFFFF" />
-          
+          <HabilidadView
+            name="Visual Studio Code"
+            src="https://img.icons8.com/?size=100&id=121601&format=png&color=FFFFFF"
+          />
         </div>
       </div>
     </section>
@@ -88,20 +132,24 @@ const tecnologiesDistributor = [
       <div class="max-w-7xl mx-auto text-center">
         <h2 class="text-3xl font-bold text-teal-400 mb-12">Proyectos Destacados</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
-       
-          <!-- Repite más bloques de proyectos como sea necesario -->
-          <ProyectPreView 
-            :image="inicioImage" 
-            name="La Viejita" 
+          <ProyectPreView
+            :image="inicioSimsDiary"
+            name="Diario los Fans Sims"
+            text="Diario online, usuarios pueden crear sus diarios, ver el de los demás, ponerle puntuación y agregarlo entre sus diarios favoritos."
+            href="/proyecto-diario-sims"
+            :technologies="tecnologiesSimsDiary"
+          />
+          <ProyectPreView
+            :image="inicioDistribuitor"
+            name="La Viejita"
             text="Distribuidora de alimentos, mayorista y minorista, carrito, hacer pedidos. 
             Lado administrativo, carga de productos, empleados, seguimiento de empleados 
-            y estadística mensual." 
+            y estadística mensual."
             href="/proyecto-destribuidora"
             :technologies="tecnologiesDistributor"
           />
         </div>
 
-        <!-- Botón para Ver Todos los Proyectos -->
         <div class="mt-12">
           <a
             href="#todos-proyectos"
---
+++ src/views/proyects/simsDiary/Index.vue
@@ -0,0 +1,51 @@
+<script setup>
+import LayoutProyects from "../layout/LayoutProyects.vue";
+import CarouselView from "../components/CarouselView.vue";
+import inicioImage from "@/assets/images/sims-page/inicio.png";
+
+const DiaryCustomerData = [
+  {
+    src: inicioImage,
+    alt: "Vista pantalla inicial",
+    description: "En la vista inicial, tenemos la bienvenida a la pagina, también se puede acceder a ver todos los días, pero si queremos entrar al perfil tenemos que estar registrados y logiados. En la seccion izquierda estan las opciones para ingresar o registrarse como usuario.",
+  },
+  {
+    src: inicioImage,
+    alt: "Vista pantalla inicial",
+    description: "En la vista inicial, tenemos la bienvenida a la pagina, también se puede acceder a ver todos los días, pero si queremos entrar al perfil tenemos que estar registrados y logiados. En la seccion izquierda estan las opciones para ingresar o registrarse como usuario.",
+  },
+];
+</script>
+<template>
+  <div class="bg-gray-900 text-white min-h-screen">
+    <LayoutProyects />
+    <section class="py-20">
+      <div class="max-w-4xl mx-auto">
+        <h2 class="text-4xl font-bold text-teal-400 mb-6">Diario los Fans Sims</h2>
+        <div class="text-lg text-gray-300 leading-relaxed mb-10">
+          <p>
+            Proyecto personal que trata de recrear la sección de historias en el sitio
+            oficial de Los Sims donde los usuarios podían registrarse para crear y leer
+            historias, agregarlas a favoritos para seguir sus actualizaciones y
+            puntuarlas.
+          </p>
+          <p>Desarrollado en php, css para los estilos y MySql para la base de datos.</p>
+          <div class="mt-5 border-t">
+            <p>
+              Si quieres ver el repositorio del proyecto:
+              <a
+                class="hover:text-teal-400"
+                href="https://github.com/FernandoAriel80/Pagina-Fans-Sims"
+                >Acá</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="max-w-5xl m-7 mx-auto">
+        <h1 class="text-3xl font-bold text-teal-400">Vista del usuario</h1>
+        <CarouselView :data="DiaryCustomerData" />
+      </div>
+    </section>
+  </div>
+</template>
