2dd40ea82225d18957321a956e1a8bfb3b5bf9a6
Fernando Orellana <fernandoarieldb95@gmail.com>
2025-01-19 17:00:57 -0300

    arreglo de scroll

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import DistributorView from '../views/proyects/distributor/Index.vue';
 import SimsDiary from '../views/proyects/simsDiary/Index.vue';
 import Workshop from '../views/proyects/workshop/Index.vue';
 
-const router = createRouter({
+/* const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -28,6 +28,39 @@ const router = createRouter({
       component: Workshop,
     },
   ],
-})
+}) */
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/proyecto-destribuidora',
+      name: 'proyect-distributor',
+      component: DistributorView,
+    },
+    {
+      path: '/proyecto-diario-sims',
+      name: 'proyect-sims-diary',
+      component: SimsDiary,
+    },
+    {
+      path: '/proyecto-taller',
+      name: 'proyect-workshop',
+      component: Workshop,
+    },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    // Verifica si hay una posición guardada (al navegar hacia atrás/adelante)
+    if (savedPosition) {
+      return savedPosition; // Mantiene la posición guardada
+    }
+    // Si no, desplázate al principio de la página
+    return { top: 0 };
+  },
+});
 export default router
